d4f0a528fa590d736c3b64fd08e09612fbc92e83
anhefti <andreas.hefti@let.ethz.ch>
2018-11-13 13:18:45 +0100

    SEBSERV-2 #Spring Boot setup

---
+++ src/main/java/ch/ethz/seb/sebserver/SEBServer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018 ETH Zürich, Educational Development and Technology (LET)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package ch.ethz.seb.sebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class SEBServer {
+
+    public static void main(final String[] args) {
+        org.apache.ibatis.logging.LogFactory.useSlf4jLogging();
+        SpringApplication.run(SEBServer.class, args);
+    }
+
+}
