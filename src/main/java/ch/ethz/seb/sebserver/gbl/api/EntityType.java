b62fb9fd58aac85295d2fca31c651f7b72d1bbef
anhefti <andreas.hefti@let.ethz.ch>
2020-07-06 10:47:51 +0200

    batis code generation with now schema (1.1.0)

--- src/main/java/ch/ethz/seb/sebserver/gbl/api/EntityType.java
+++ src/main/java/ch/ethz/seb/sebserver/gbl/api/EntityType.java
@@ -2,7 +2,7 @@ package ch.ethz.seb.sebserver.gbl.api;
 
 import javax.annotation.Generated;
 
-@Generated(value="org.mybatis.generator.api.MyBatisGenerator",comments="ch.ethz.seb.sebserver.gen.DomainModelNameReferencePlugin",date="2020-02-05T10:36:21.905+01:00")
+@Generated(value="org.mybatis.generator.api.MyBatisGenerator",comments="ch.ethz.seb.sebserver.gen.DomainModelNameReferencePlugin",date="2020-07-06T10:44:00.903+02:00")
 public enum EntityType {
     CONFIGURATION_ATTRIBUTE,
     CONFIGURATION_VALUE,
--- src/main/java/ch/ethz/seb/sebserver/webservice/datalayer/batis/mapper/WebserviceServerInfoRecordDynamicSqlSupport.java
+++ src/main/java/ch/ethz/seb/sebserver/webservice/datalayer/batis/mapper/WebserviceServerInfoRecordDynamicSqlSupport.java
@@ -6,19 +6,19 @@ import org.mybatis.dynamic.sql.SqlColumn;
 import org.mybatis.dynamic.sql.SqlTable;
 
 public final class WebserviceServerInfoRecordDynamicSqlSupport {
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-02-05T10:36:22.035+01:00", comments="Source Table: webservice_server_info")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-07-06T10:44:01.048+02:00", comments="Source Table: webservice_server_info")
     public static final WebserviceServerInfoRecord webserviceServerInfoRecord = new WebserviceServerInfoRecord();
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-02-05T10:36:22.035+01:00", comments="Source field: webservice_server_info.id")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-07-06T10:44:01.048+02:00", comments="Source field: webservice_server_info.id")
     public static final SqlColumn<Long> id = webserviceServerInfoRecord.id;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-02-05T10:36:22.035+01:00", comments="Source field: webservice_server_info.uuid")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-07-06T10:44:01.049+02:00", comments="Source field: webservice_server_info.uuid")
     public static final SqlColumn<String> uuid = webserviceServerInfoRecord.uuid;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-02-05T10:36:22.035+01:00", comments="Source field: webservice_server_info.service_address")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-07-06T10:44:01.049+02:00", comments="Source field: webservice_server_info.service_address")
     public static final SqlColumn<String> serviceAddress = webserviceServerInfoRecord.serviceAddress;
 
-    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-02-05T10:36:22.035+01:00", comments="Source Table: webservice_server_info")
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2020-07-06T10:44:01.048+02:00", comments="Source Table: webservice_server_info")
     public static final class WebserviceServerInfoRecord extends SqlTable {
         public final SqlColumn<Long> id = column("id", JDBCType.BIGINT);
 
