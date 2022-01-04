26aea16b25e629949c2c621041918b24106a298b
anhefti <andreas.hefti@let.ethz.ch>
2022-01-04 11:11:32 +0100

    SEBSERV-255

--- src/main/resources/ch/ethz/seb/sebserver/gui/service/session/proctoring/openRoomScript.js
+++ src/main/resources/ch/ethz/seb/sebserver/gui/service/session/proctoring/openRoomScript.js
@@ -1,7 +1,7 @@
 try {
     var existingWin = window.open('', '%s', 'height=%s,width=%s,location=no,scrollbars=yes,status=no,menubar=0,toolbar=no,titlebar=no,dialog=no');
     if (existingWin == null || typeof(existingWin)=='undefined') {  
-        alert('Please disable your pop-up blocker and try again.'); 
+        alert('Please disable your pop-up blocker and try again.');
     } else {
         if(existingWin.location.href === 'about:blank') {
             existingWin.document.title = '%s';
