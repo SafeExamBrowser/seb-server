bcb4a867a535e243035f091453e3e31fe70dad8b
anhefti <andreas.hefti@let.ethz.ch>
2022-02-10 14:21:31 +0100

    fixed default value

---
+++ src/main/resources/config/sql/base/V14__fixSEBSettingsDefault_v1_3.sql
@@ -0,0 +1,5 @@
+-- -----------------------------------------------------
+-- FIX SEB Settings default
+-- -----------------------------------------------------
+
+UPDATE configuration_attribute SET default_value='true' WHERE id=974;
