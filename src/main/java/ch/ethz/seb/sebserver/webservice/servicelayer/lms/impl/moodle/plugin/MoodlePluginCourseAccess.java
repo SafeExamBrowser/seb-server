3ce025c4b1ba9d79e0b10f9d9032c418199fa65e
anhefti <andreas.hefti@let.ethz.ch>
2023-06-02 10:52:52 +0200

    SEBSERV-449 better timeouts and name search on moodle side

--- src/main/java/ch/ethz/seb/sebserver/webservice/servicelayer/lms/impl/moodle/plugin/MoodlePluginCourseAccess.java
+++ src/main/java/ch/ethz/seb/sebserver/webservice/servicelayer/lms/impl/moodle/plugin/MoodlePluginCourseAccess.java
@@ -61,6 +61,7 @@ import ch.ethz.seb.sebserver.webservice.servicelayer.lms.impl.moodle.MoodleUtils
 import ch.ethz.seb.sebserver.webservice.servicelayer.lms.impl.moodle.MoodleUtils.Courses;
 import ch.ethz.seb.sebserver.webservice.servicelayer.lms.impl.moodle.MoodleUtils.CoursesPlugin;
 import ch.ethz.seb.sebserver.webservice.servicelayer.lms.impl.moodle.MoodleUtils.MoodleUserDetails;
+import io.micrometer.core.instrument.util.StringUtils;
 
 public class MoodlePluginCourseAccess extends AbstractCachedCourseAccess implements CourseAccessAPI {
 
@@ -118,7 +119,7 @@ public class MoodlePluginCourseAccess extends AbstractCachedCourseAccess impleme
                 environment.getProperty(
                         "sebserver.webservice.circuitbreaker.moodleRestCall.blockingTime",
                         Long.class,
-                        Constants.SECOND_IN_MILLIS * 20),
+                        Constants.SECOND_IN_MILLIS * 30),
                 environment.getProperty(
                         "sebserver.webservice.circuitbreaker.moodleRestCall.timeToRecover",
                         Long.class,
@@ -184,10 +185,11 @@ public class MoodlePluginCourseAccess extends AbstractCachedCourseAccess impleme
                 quizFromTime = DateTime.now(DateTimeZone.UTC).minusYears(this.cutoffTimeOffset);
             }
             final Predicate<QuizData> quizFilter = LmsAPIService.quizFilterPredicate(filterMap);
+            final String quizName = filterMap.getQuizName();
 
             while (!asyncQuizFetchBuffer.finished && !asyncQuizFetchBuffer.canceled) {
                 try {
-                    fetchQuizzesPage(page, quizFromTime, asyncQuizFetchBuffer, quizFilter);
+                    fetchQuizzesPage(page, quizFromTime, quizName, asyncQuizFetchBuffer, quizFilter);
                     page++;
                 } catch (final Exception e) {
                     log.error("Unexpected error while trying to fetch moodle quiz page: {}", page, e);
@@ -371,6 +373,7 @@ public class MoodlePluginCourseAccess extends AbstractCachedCourseAccess impleme
     private void fetchQuizzesPage(
             final int page,
             final DateTime quizFromTime,
+            final String nameCondition,
             final AsyncQuizFetchBuffer asyncQuizFetchBuffer,
             final Predicate<QuizData> quizFilter) throws JsonParseException, JsonMappingException, IOException {
 
@@ -382,7 +385,7 @@ public class MoodlePluginCourseAccess extends AbstractCachedCourseAccess impleme
                 : lmsSetup.lmsApiUrl + Constants.URL_PATH_SEPARATOR + MOODLE_QUIZ_START_URL_PATH;
 
         final Collection<CourseData> fetchCoursesPage =
-                fetchCoursesPage(restTemplate, quizFromTime, page, this.pageSize);
+                fetchCoursesPage(restTemplate, quizFromTime, nameCondition, page, this.pageSize);
         // finish if page is empty (no courses left
         if (fetchCoursesPage.isEmpty()) {
             asyncQuizFetchBuffer.finish();
@@ -408,6 +411,7 @@ public class MoodlePluginCourseAccess extends AbstractCachedCourseAccess impleme
     private Collection<CourseData> fetchCoursesPage(
             final MoodleAPIRestTemplate restTemplate,
             final DateTime quizFromTime,
+            final String nameCondition,
             final int page,
             final int size) throws JsonParseException, JsonMappingException, IOException {
 
@@ -422,12 +426,18 @@ public class MoodlePluginCourseAccess extends AbstractCachedCourseAccess impleme
             final long defaultCutOff = Utils.toUnixTimeInSeconds(
                     DateTime.now(DateTimeZone.UTC).minusYears(this.cutoffTimeOffset));
             final long cutoffDate = (filterDate < defaultCutOff) ? filterDate : defaultCutOff;
-            final String sqlCondition = String.format(
+            String sqlCondition = String.format(
                     SQL_CONDITION_TEMPLATE,
                     String.valueOf(cutoffDate),
                     String.valueOf(filterDate));
             final String fromElement = String.valueOf(page * size);
             final LinkedMultiValueMap<String, String> attributes = new LinkedMultiValueMap<>();
+
+            if (StringUtils.isNotBlank(nameCondition)) {
+                sqlCondition = sqlCondition + " AND (m.name LIKE '" +
+                        Utils.toSQLWildcard(nameCondition) +
+                        "')";
+            }
 
             // Note: courseid[]=0 means all courses. Moodle don't like empty parameter
             attributes.add(PARAM_COURSE_ID_ARRAY, "0");
