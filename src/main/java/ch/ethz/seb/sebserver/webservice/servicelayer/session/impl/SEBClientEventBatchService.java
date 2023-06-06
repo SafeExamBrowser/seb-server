8322f07cf7a6e35118d21c13571e5f38978aad34
anhefti <andreas.hefti@let.ethz.ch>
2023-06-06 09:06:02 +0200

    with logging

--- src/main/java/ch/ethz/seb/sebserver/webservice/servicelayer/session/impl/SEBClientEventBatchService.java
+++ src/main/java/ch/ethz/seb/sebserver/webservice/servicelayer/session/impl/SEBClientEventBatchService.java
@@ -135,9 +135,9 @@ public class SEBClientEventBatchService {
     private void processEvents(final String workerName, final Collection<EventData> events) {
 
         long start = 0L;
-        if (log.isDebugEnabled()) {
-            start = Utils.getMillisecondsNow();
-        }
+        //if (log.isDebugEnabled()) {
+        start = Utils.getMillisecondsNow();
+        //}
 
         final int size = this.eventDataQueue.size();
         if (size > 1000) {
@@ -176,12 +176,12 @@ public class SEBClientEventBatchService {
 
             this.sqlSessionTemplate.flushStatements();
 
-            if (log.isDebugEnabled()) {
-                log.debug("SEBClientEventBatchService worker {} processes batch of size {} in {} ms",
-                        workerName,
-                        size,
-                        start - Utils.getMillisecondsNow());
-            }
+            //if (log.isDebugEnabled()) {
+            log.debug("SEBClientEventBatchService worker {} processes batch of size {} in {} ms",
+                    workerName,
+                    size,
+                    start - Utils.getMillisecondsNow());
+            //}
 
         } catch (final Exception e) {
             log.error("Failed to process SEB events from eventDataQueue: ", e);
