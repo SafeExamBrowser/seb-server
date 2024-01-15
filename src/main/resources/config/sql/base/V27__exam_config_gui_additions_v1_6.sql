9a21ba25c8c8c35a16d53d901a2e8f4bde4ad691
anhefti <andreas.hefti@let.ethz.ch>
2024-01-15 10:35:45 +0100

    SEBSERV-465 SEB Settings GUI migration

--- src/main/resources/config/sql/base/V27__exam_config_gui_additions_v1_6.sql
+++ src/main/resources/config/sql/base/V27__exam_config_gui_additions_v1_6.sql
@@ -1,4 +1,26 @@
 -- ----------------------------------------------------------------
--- Add SEB Settings GUI additions (SEBSERV-414 and SEBSERV-465)
+-- Repair SEB Settings signature to be child of prohibitedProcesses
+-- ----------------------------------------------------------------
+
+UPDATE configuration_attribute SET name="permittedProcesses.signature", parent_id=73 WHERE id=1200;
+
+-- ----------------------------------------------------------------
+-- New Settings from issue SEBSERV-501
+-- ----------------------------------------------------------------
+
+INSERT IGNORE INTO configuration_attribute VALUES
+    (1580, 'allowUploads', 'CHECKBOX', null, null, null, null, 'true'),
+    (1581, 'allowDownloads', 'CHECKBOX', null, null, null, null, 'true')
+;
+
+-- ----------------------------------------------------------------
+-- Add SEB Settings GUI additions (SEBSERV-465)
+-- ----------------------------------------------------------------
+
+INSERT IGNORE INTO orientation (config_attribute_id, template_id, view_id, group_id, x_position, y_position, width, height, title) VALUES
+    (1564, 0,  3, null, 7, 15, 5, 1, 'TOP');
+
+-- ----------------------------------------------------------------
+-- Add SEB Settings GUI additions (SEBSERV-414)
 -- ----------------------------------------------------------------
 
