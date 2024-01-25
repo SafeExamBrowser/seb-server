964b6c7e56f48cd4386da860247d19609ff10f8d
anhefti <andreas.hefti@let.ethz.ch>
2024-01-25 10:15:33 +0100

    SEBSERV-502 deprecation note

--- docs/conf.py
+++ docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
