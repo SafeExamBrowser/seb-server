c2557fff751a00c63fd3315d620751dd29be809f
anhefti <andreas.hefti@let.ethz.ch>
2025-03-05 14:06:07 +0100

    SEBSERV-651 for testing fixed logs

--- src/main/java/ch/ethz/seb/sebserver/webservice/servicelayer/lms/LmsAPIService.java
+++ src/main/java/ch/ethz/seb/sebserver/webservice/servicelayer/lms/LmsAPIService.java
@@ -116,15 +116,15 @@ public interface LmsAPIService {
             final boolean nameFilter = StringUtils.isBlank(name) || (q.name != null && q.name.contains(name));
 
             // new quiz start date filter SEBSERV-651
-            if (from != null) {
-                log.info("*%%%%%%%%%%%%%%%% dayStart: " +  dayStart + " startDay_Quiz: " + q.startTime.withTime(0, 0, 0, 0).getMillis());
-            }
+//            if (from != null) {
+//                log.info("*%%%%%%%%%%%%%%%% dayStart: " +  dayStart + " startDay_Quiz: " + q.startTime.withTime(0, 0, 0, 0).getMillis());
+//            }
             boolean startTimeFilter = true;
             if (dayStart != null) {
 //                final long dayStart = from.withTime(0, 0, 0, 0).getMillis();
 //                final long dayEnd = from.withTime(23, 59, 59, 999).getMillis();
                 final long quizStart = q.startTime.getMillis();
-                log.info("***************** quizStart: " +  quizStart);
+                log.info("***************** quiz: " + q.name + " quizStart: " +  q.startTime + " quizStartMillis: " + quizStart);
                 startTimeFilter = dayStart <= quizStart && dayEnd >= quizStart;
             }
             
