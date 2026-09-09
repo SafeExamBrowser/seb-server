b7fe2d15f0e8f42f440bb5a94855f55be63fa35e
Andreas Hefti <andreas.hefti@id.ethz.ch>
2026-09-09 14:58:11 +0200

    SEBSERV-1006 fixed scheduled deletion table

---
+++ src/main/resources/config/sql/base/V43__fixScheduledDeletion_v3_0.sql
@@ -0,0 +1,7 @@
+-- -----------------------------------------------------
+-- Table `scheduled_delete_info` exam_uuid column size
+-- -----------------------------------------------------
+
+ALTER TABLE `scheduled_delete_info`
+MODIFY `exam_uuid` VARCHAR(255) NULL
+;
